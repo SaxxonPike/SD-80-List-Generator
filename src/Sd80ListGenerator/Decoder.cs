142f8fd3977482df1722a08619ba6dfd88c00704
Saxxon Fox <saxxonpike@gmail.com>
2022-08-03 16:55:10 -0500

    Better CSV export and examples

--- src/Sd80ListGenerator/Decoder.cs
+++ src/Sd80ListGenerator/Decoder.cs
@@ -4,21 +4,26 @@ namespace Sd80ListGenerator;
 
 public static class Decoder
 {
+    private static string FixQuotes(string s)
+    {
+        return s.Replace("\"", "\"\"");
+    }
+    
     public static void DecodeWaveformList(ReadOnlySpan<byte> bytes, TextWriter writer)
     {
-        writer.WriteLine("Index, Name");
+        writer.WriteLine("Index,Name");
         for (var i = (bytes.Length - 1) / 16 * 16; i >= 0; i -= 16)
         {
             var record = bytes.Slice(i, 16);
-            var idx = (bytes.Length - i) / 16;
+            var idx = ((bytes.Length - i) / 16) - 1;
             var name = Encoding.ASCII.GetString(record[..12]).Trim();
-            writer.WriteLine($"{idx - 1:D3},   {name}");
+            writer.WriteLine($"{idx},{FixQuotes(name)}");
         }
     }
 
     public static void DecodePatchList(ReadOnlySpan<byte> bytes, TextWriter writer)
     {
-        writer.WriteLine("Index, MSB,   LSB,   PRG,   Name");
+        writer.WriteLine("Index,MSB,LSB,PRG,Name");
         for (var i = 0; i < bytes.Length; i += 16)
         {
             var record = bytes.Slice(i, 16);
@@ -27,7 +32,7 @@ public static class Decoder
             var lsb = record[1];
             var prg = record[2] + 1;
             var name = Encoding.ASCII.GetString(record.Slice(3, 12)).Trim();
-            writer.WriteLine($"{idx:D4},  {msb:D3},   {lsb:D3},   {prg:D3},   {name}");
+            writer.WriteLine($"{idx},{msb},{lsb},{prg},{FixQuotes(name)}");
         }
     }
 }
